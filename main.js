f93904baa31a10e41ffb55bb5f7a309e7898af39
JehZucco <andressa0360@gmail.com>
2024-10-30 14:20:33 +0100

    adicionando lógica para 10 tentativas

--- main.js
+++ main.js
@@ -1,18 +1,52 @@
 
- function testaLogica(){
+ //gerar número aleatório
+ var numeroAleatorio = Math.random() * 100;
 
-    var recebeNumero = document.getElementById("recebe_numero");
+ var numeroAleatorioArredondado =  Math.round(numeroAleatorio);
+ 
+ //Determina o número de tentativas
+ var tentativasRestantes = 10;
+ 
+ function jogoAdivinhaNumero(event){
 
-    var numeroAleatorio = Math.random() * 10;
-    
+    tentativasRestantes--;
+
+    if(tentativasRestantes === 0){
+        alert('Fim de jogo! O número era: ' + numeroAleatorioArredondado + '. Um novo número foi gerado, você pode tentar novamente');
+        document.getElementById("recebe_numero").value = "";
+        location.reload();
+        }
+
+    //Insere o número de tentativas atual:
+    var insereNumerodeTentativas = document.getElementById('tentativas');
+    insereNumerodeTentativas.textContent = 'Número de tentativas: ' + tentativasRestantes;
+
+    //Impede que a página atualize ao submeter o formulário
+    event.preventDefault();
+
+    //Pega o número digitado pelo usuário
+    var recebeNumero = document.getElementById("recebe_numero").value;
    
-    var numeroAleatorioArredondado =  Math.round(numeroAleatorio);
 
-    if(recebeNumero.value == numeroAleatorioArredondado){
-        alert("Acertou!");
-    }else{
-        alert("Errou... o número era " + numeroAleatorioArredondado + " / Você digitou " + recebeNumero.value);
-   }
- }
+    //Pega o parágrafo pra manipular e colocar a dica
+    var insereDica = document.getElementById('dica');
+
+        
+
+        if(recebeNumero > numeroAleatorioArredondado){
+            insereDica.textContent = 'É um número menor que ' + recebeNumero + " " + numeroAleatorioArredondado;
+        }else if(recebeNumero < numeroAleatorioArredondado){
+           insereDica.textContent = 'É um número maior que ' + recebeNumero + " " + numeroAleatorioArredondado;
+       }else if (recebeNumero == numeroAleatorioArredondado){
+            alert('Acertou! Um novo número foi gerado, você pode tentar novamente');
+            document.getElementById("recebe_numero").value = "";
+            location.reload();
+        }     
+    
+}
+
+
+
+ document.getElementById('adivinhaNumero').addEventListener('submit', jogoAdivinhaNumero)
 
 
